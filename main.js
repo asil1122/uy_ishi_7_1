56a2c84a33fc88e854b269b81ae2e14804127693
asil1122 <asilbekabdurashidov000gmail.com>
2024-11-26 00:07:47 +0500

    finish

---
+++ main.js
@@ -0,0 +1,110 @@
+"use strict";
+// 1- masalla //
+// let soz: string | null = prompt("Enter a string");
+// let a: string = '';
+// if(soz != null){
+//   a += '<<';
+//   for(let i = 0; i < soz.length; i++){
+//     a += soz[i];
+//   }
+//   a += '>>';
+// }
+// console.log(a);
+// 2- masalla //
+// let a: string | null = prompt("Enter a string");
+// let num: number = Number(prompt("Enter a number"));
+// let b: string = '';
+// if(a != null && num != null){
+//   for(let i = 0; i < num; i++){
+//     b += a[i]
+//   }
+// }
+// console.log(b);
+// 3- masalla //
+// let soz1: string | null = prompt("Enter a string");
+// let soz2: string | null = prompt("Enter a string");
+// let soz3: string =''
+// if(soz1 != null && soz2 != null){
+//   for(let i = 1; i < soz1.length; i++){
+//     soz3 += soz1[i]
+//   }
+//   for(let i = 1; i < soz2.length; i++){
+//     soz3 += soz2[i]
+//   }
+// }
+// console.log(soz3);
+// 4- masalla //
+// const str1: string | null = prompt("Enter the word");
+// const str2: string | null = prompt("enter boolean");
+// let bol: Boolean = true;
+// if (str1 != null && str2 != null) {
+//   if (str2 != "true") {
+//     bol = false;
+//   }
+//   if (bol) {
+//     console.log(str1[0]);
+//   } else {
+//     console.log(str1[str1.length - 1]);
+//   }
+// }
+// 5- masalla //
+// let soz1: string | null = prompt("Enter a string");
+// if (soz1 != null) {
+//   if (soz1[soz1.length - 1] == 'y' && soz1[soz1.length - 2] == 'l') {
+//     console.log(Boolean(true));
+//   } else {
+//     console.log(Boolean(false));
+//   }
+// }
+// 6- masalla //
+// let soz1: string | null = prompt("Enter a string");
+// let soz2: string | null = prompt("Enter a string");
+// let a: string = '';
+// if (soz1 != null && soz2 != null) {
+//   if(soz1 == '' || soz2 == '' ){
+//     a += '@';
+//   }else{
+//     a += soz1[0] + soz2[soz2.length - 1];
+//   }
+// } 
+// console.log(a);
+// 7- masalla //
+// let soz1: string | null = prompt("Enter a string");
+// if (soz1 != null) {
+//     if (soz1[0] == 'b' && soz1[1] == 'l' && soz1[2] == 'u' && soz1[3] == 'e') {
+//       console.log('blue');
+//     }else if(soz1[0] == 'r' && soz1[1] == 'e' && soz1[2] == 'd'){
+//       console.log('red');
+//   }else{
+//     console.log('');
+//   }
+// }
+// 8- masalla //
+// let soz1: string | null = prompt("Enter a string");
+// let num: number = Number(prompt("Enter a number"));
+// let a: string = '';
+// if (soz1 != null) {
+//   for (let i = 0; i < num; i++) {
+//     a += soz1[0]+soz1[1] 
+//   }
+// }
+// console.log(a);
+// 9- masalla //
+// let soz1: string | null = prompt("Enter a string");
+// let num: number = Number(prompt("Enter a number"));
+// let a: string =''
+// if (soz1 != null) {
+//   for (let i = 0; i < num; i++) {
+//     a += soz1[soz1.length - 2]+ soz1[soz1.length - 1]
+//   }
+// }
+// console.log(a);
+// 10- masalla //
+// let soz1: string | null = prompt("Enter a string");
+// if (soz1 != null) {
+//   if(soz1[0] && soz1 [1] ==  soz1[soz1.length - 1] && soz1[soz1.length - 2] ){
+//     console.log(Boolean(true));
+//   }else{
+//     console.log(Boolean(false));
+//   }
+// }
